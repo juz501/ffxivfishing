163682ea9b6c3151ad5148bb4c126b9ea8c69df8
Julian <julianc@gmail.com>
2020-07-03 20:59:35 +1000

    try client-side login with nickname

--- components/cnav.vue
+++ components/cnav.vue
@@ -7,8 +7,37 @@
         <li><nuxt-link to="/fish-db">Fish Database</nuxt-link></li>
       </ul>
     </nav>
+    <nav>
+      <ul>
+        <li v-if="loggedIn !== true"><a @click="login" href='#'>Login</a></li>
+        <li v-if="loggedIn === true"><span>Logged In as {{ nickname }}</span>
+        <li v-if="loggedIn === true"><a @click="logout" href='#'>Logout</a></li>
+      </ul>
+    </nav>
   </div>
 </template>
+<script>
+export default {
+  data: {
+    loggedIn: false,
+    nickname: ''
+  },
+  methods: {
+    login() {
+      if ( process.client && window && window.netlifyIdentity )
+        const user = window.netlifyIdentity.currentUser();
+        if ( user ) {
+          this.loggedIn = true;
+          this.nickname = user.user_metadata.full_name;
+        } else {
+          this.loggedIn = false;
+          this.nickname = '';
+        }
+      }
+    }
+  }
+}
+</script>
 <style scoped>
   .menu-bar {
     display: flex;
