153c655ab2f5876ac4051af93bfe91d1158cdfb9
Julian <julianc@gmail.com>
2020-07-03 22:08:28 +1000

    escape windows

--- store/user.js
+++ store/user.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 
 export const state = () => (
   {
-    currentUser: window.localStorage.getItem( 'user' )
+    currentUser: ( process.client && window ) ? window.localStorage.getItem( 'user' ) : null
   }
 )
 
@@ -16,11 +16,15 @@ export const mutations = {
   SET_USER( state, {id, data} ) {
     if ( !data ) {
       Vue.set( state, id, null );
-      window.localStorage.removeItem( 'user' );
+      if ( process.client && window ) {
+        window.localStorage.removeItem( 'user' );
+      }
       return;
     }
     let theUser = JSON.stringify( data );
     Vue.set( state , id, data );
-    window.localStorage.setItem( 'user', theUser );
+    if ( process.client && window ) {
+      window.localStorage.setItem( 'user', theUser );
+    }
   }  
 }
