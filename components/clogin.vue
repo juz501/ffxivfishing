a7bafa3a5c57fda041ef95359e122291ed527854
Julian <julianc@gmail.com>
2020-07-03 23:20:09 +1000

    map state to get user

--- components/clogin.vue
+++ components/clogin.vue
@@ -1,11 +1,14 @@
 <template>
   <nav>
     <ul>
+      <li v-if="currentUser">{{ fullName }}</li>
       <li data-netlify-identity-button></li>
     </ul>
   </nav>
 </template>
 <script>
+import { mapState } from 'vuex';
+
 export default {
   mounted() {
     if ( process.client && window.netlifyIdentity ) {
@@ -13,6 +16,10 @@ export default {
       ni.on( 'login', user => this.$store.dispatch( 'setUser', user));
       ni.on( 'logout', () => this.$store.dispatch( 'setUser', null));
     }
-  }
+  },
+  computed: mapState({
+    currentUser: state => state.currentUser,
+    fullName: state => state.currentUser ? state.currentUser.user_metadata.full_name : ''
+  })
 }
 </script>
