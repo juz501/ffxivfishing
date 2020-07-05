4da5e61259adcc20251078e13af6d3147caa5453
Julian <julianc@gmail.com>
2020-07-05 18:50:22 +1000

    add error checking

--- functions/sisipu/sisipu.js
+++ functions/sisipu/sisipu.js
@@ -6,7 +6,23 @@ exports.handler = ( event, context, callback ) => {
     });
     return;
   }
-  const payload = JSON.parse( event.body ).payload
+  if ( ! event.body ) {
+    callback( null, {
+      statusCode: 200,
+      body: "Hello, Moogle with no letters"
+    });
+    return;
+  }
+  try {
+    const body = JSON.parse( event.body );
+    const payload = body.payload;
+  } catch (e) {
+    callback( null, {
+      statusCode: 200,
+      body: "Hello, Fake Moogle"
+    });
+    return;
+  }
   callback( null, {
     statusCode: 200,
     body: "Hello, CID: " + payload
