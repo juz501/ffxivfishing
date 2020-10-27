a1eb3e99d081a31ff2594e3817a20f0b27eb1752
Julian <julianc@gmail.com>
2020-10-27 20:37:18 +1100

    add some rows in

--- pages/fish-leves/index.vue
+++ pages/fish-leves/index.vue
@@ -19,6 +19,116 @@
           <td>Unsynwilf (x10, y8)</td>
           <td>Lower La Noscea - Limsa Lominsa</td>          
         </tr>
+        <tr>
+          <td>Soup to Guts - Level 1</td>
+          <td>Wyrkholsk	(x31, y20)</td>
+          <td>Lower La Noscea	- Limsa Lominsa</td>
+          <td>Merlthor Goby	x3 (10,980 xp)<td>
+          <td>Unsynwilf (x10, y8)</td>
+          <td>Lower La Noscea - Limsa Lominsa</td>
+        </tr>
+        <tr>
+          <td>A Kelping Hand - Level 1</td>
+          <td>Wyrkholsk	(x31, y20)</td>
+          <td>Lower La Noscea	- Limsa Lominsa</td>
+          <td>Malm Kelp	x3 (860 xp)<td>
+          <td>Unsynwilf (x10, y8)</td>
+          <td>Lower La Noscea - Limsa Lominsa</td>
+        </tr>
+        <tr>
+          <td>Poor Boys Eat Well - Level 1</td>
+          <td>Wyrkholsk	(x31, y20)</td>
+          <td>Lower La Noscea	- Limsa Lominsa</td>
+          <td>Finger Shrimp	x3 (860 xp)<td>
+          <td>Unsynwilf (x10, y8)</td>
+          <td>Lower La Noscea - Limsa Lominsa</td>
+        </tr>
+        <tr>
+          <td>Splendor in the Glass - Level 5</td>
+          <td>Wyrkholsk	(x31, y20)</td>
+          <td>Lower La Noscea	- Limsa Lominsa</td>
+          <td>Harbor Herring x3 (28,431 xp)<td>
+          <td>Unsynwilf (x10, y8)</td>
+          <td>Lower La Noscea - Limsa Lominsa</td>
+        </tr>
+        <tr>
+          <td>Splendor in the Glass - Level 5</td>
+          <td>Wyrkholsk	(x31, y20)</td>
+          <td>Lower La Noscea	- Limsa Lominsa</td>
+          <td>Ocean Cloud x3 (1,053 xp)<td>
+          <td>Unsynwilf (x10, y8)</td>
+          <td>Lower La Noscea - Limsa Lominsa</td>
+        </tr>
+        <tr>
+          <td>They Taste Just as Pretty - Level 5</td>
+          <td>Wyrkholsk	(x31, y20)</td>
+          <td>Lower La Noscea	- Limsa Lominsa</td>
+          <td>Coral Butterfly x3 (31,590 xp)<td>
+          <td>Unsynwilf (x10, y8)</td>
+          <td>Lower La Noscea - Limsa Lominsa</td>
+        </tr>
+        <tr>
+          <td>Yummy in the Tummy - Level 5</td>
+          <td>Wyrkholsk	(x31,y 20)</td>
+          <td>Lower La Noscea	- Limsa Lominsa</td>
+          <td>Sea Cucumber x3 (1,137 xp)<td>
+          <td>Unsynwilf (x10, y8)</td>
+          <td>Lower La Noscea - Limsa Lominsa</td>
+        </tr>
+        <tr>
+          <td>Fish by Many Other Names - Level 10</td>
+          <td>Swygskyf (x34, y31)</td>
+          <td>Western La Noscea - Swiftperch</td>
+          <td>Moraby Flounder x3 (2,022 xp)<td>
+          <td>Q'molosi (x34, y31)</td>
+          <td>Western La Noscea - Swiftperch</td>
+        </tr>
+        <tr>
+          <td>Just Add Water - Level 10</td>
+          <td>Swygskyf (x34, y31)</td>
+         <td>Western La Noscea - Swiftperch</td>
+          <td>Tiger Cod x3 (60,660 xp)<td>
+          <td>Q'molosi (x34, y31)</td>
+          <td>Western La Noscea - Swiftperch</td>
+        </tr>
+        <tr>
+          <td>The Fertile Incandescent - Level 10</td>
+          <td>Swygskyf (x34, y31)</td>
+          <td>Western La Noscea - Swiftperch</td>
+          <td>Harbor Herring x3 (54,594 xp)<td>
+          <td>Q'molosi (x34, y31)</td>
+          <td>Western La Noscea - Swiftperch</td>
+        </tr>
+<!--Brain Candy	Sincerity	10	Swygskyf	Western La Noscea	Swiftperch	(x34,y31)	2,410	103	Pebble Crab	Deliver Pebble Crabs to Q'molosi. 0/3
+The Deepest Cut	Concord	15	Orwen	Western La Noscea	Aleport	(x27,y27)	12,218	156	Razor Clam	Deliver Razor Clams to Fupepe.
+The Moral of the Coral	Sincerity	15	Orwen	Western La Noscea	Aleport	(x27,y27)	3,872	142	White Coral	Deliver pieces of White Coral to Fupepe.
+Crab Life by the Horns	Sincerity	15	Orwen	Western La Noscea	Aleport	(x27,y27)	4,057	158	Helmet Crab	Deliver Helmet Crabs to Fupepe.
+Shell Game (Fisher)	Concord	15	Orwen	Western La Noscea	Aleport	(x27,y27)	4,227	162	Rothlyt Oyster	Deliver Rothlyt Oysters to Fupepe.
+Sounds Fishy to Me	Concord	20	Nyell	South Shroud	Quarrymill	(x25,y20)	12,218	210	Copperfish	Deliver copperfish to Daca Jinjahl. 0/3
+Blind Ambition	Sincerity	20	Nyell	South Shroud	Quarrymill	(x25,y20)	6,109	196-215	Dark Sleeper	Deliver dark sleepers to Daca Jinjahl. 0/3
+The Long and the Shortcrust	Concord	20	Nyell	South Shroud	Quarrymill	(x25,y20)	18,524	202-212	Black Eel	Deliver black eels to Daca Jinjahl. 0/3
+Food Chain Reaction	Sincerity	20	Nyell	South Shroud	Quarrymill	(x25,y20)	6,109	196-215	Faerie Bass	Deliver faerie bass to Daca Jinjahl. 0/3
+Fishing 101	Sincerity	25	Nyell	South Shroud	Quarrymill	(x25,y20)	9,262	235-253	Dark Bass	Deliver dark bass to Daca Jinjahl. 0/3
+A Shocking Soiree	Concord	25	Nyell	South Shroud	Quarrymill	(x25,y20)	11,033	233	Black Ghost	Deliver black ghosts to Daca Jinjahl. 0/3
+A Watery Web of Lies	Concord	25	Nyell	South Shroud	Quarrymill	(x25,y20)	29,450	233-273	Yugr'am Salmon	Deliver Yugr'am salmon to Daca Jinjahl. 0/3
+The Truth Will Set You Free	Sincerity	25	Nyell	South Shroud	Quarrymill	(x25,y20)	9,885	225-253	Five-ilm Pleco	Deliver five-ilm pleco to Daca Jinjahl. 0/3
+The Blue Period	Sincerity	30	Nahctahr	Eastern La Noscea	Costa del Sol	(x30,y30)	14,725	279-330	Sea Pickle	Deliver Sea Pickles to F'abodji. 0/3
+Kitchen Nightmares No More	Sincerity	30	Nahctahr	Eastern La Noscea	Costa del Sol	(x30,y30)	14,725	291	Black Sole	Deliver Black Sole to F'abodji. 0/3
+A Recipe for Disaster	Concord	30	Nahctahr	Eastern La Noscea	Costa del Sol	(x30,y30)	31,098	300	Ash Tuna	Deliver Ash Tuna to F'abodji. 0/3
+Just Call Me Late for Dinner	Concord	30	Nahctahr	Eastern La Noscea	Costa del Sol	(x30,y30)	30,282	285	Indigo Herring	Deliver Indigo Herring to F'abodji. 0/3
+Fry Me a River	Sincerity	35	Cimeaurant	Coerthas Central Highlands	First Dicasterial Observatorium of Aetherial and Astrological Phenomena	(x26,y28)	21,057	319-361	Giant Bass	Deliver Giant Bass to Drividot. 0/3
+The Perks of Politics	Concord	35	Cimeaurant	Coerthas Central Highlands	First Dicasterial Observatorium of Aetherial and Astrological Phenomena	(x26,y28)	19,950	322-343	Seema	Deliver Seema to Drividot. 0/3
+Gathering Light	Sincerity	35	Cimeaurant	Coerthas Central Highlands	First Dicasterial Observatorium of Aetherial and Astrological Phenomena	(x26,y28)	19,950	364	Lamp Marimo	Deliver Lamp Marimo to Drividot. 0/3
+Hands Off Our Fish	Concord	35	Cimeaurant	Coerthas Central Highlands	First Dicasterial Observatorium of Aetherial and Astrological Phenomena	(x26,y28)	26,707	319	Crimson Trout	Deliver Crimson Trout to Drividot. 0/3
+Empire Builder	Concord	40	Voilinaut	Coerthas Central Highlands	Whitebrim Front	(x12,y16)	54,214	380	Bronze Lake Trout	Deliver Bronze Lake Trout to Louviaune. 0/3
+Laird of the Lakes	Concord	40	Voilinaut	Coerthas Central Highlands	Whitebrim Front	(x12,y16)	65,144	360	Northern Pike	Deliver Northern Pike to Louviaune. 0/3
+Make a Fish	Sincerity	40	Voilinaut	Coerthas Central Highlands	Whitebrim Front	(x12,y16)	26,707	364-392	Abalathian Smelt	Deliver Abalathian Smelts to Louviaune. 0/3
+Rationally Speaking	Sincerity	40	Voilinaut	Coerthas Central Highlands	Whitebrim Front	(x12,y16)	26,707	396	Golden Loach	Deliver Golden Loach to Louviaune. 0/3
+Snail Fail	Sincerity	45	K'leytai	Mor Dhona	Saint Coinach's Find	(x29,y12)	32,572	464	Ignus Snail	Deliver Ignus Snails to Clifton. 0/3
+Awash in Evidence	Concord	45	K'leytai	Mor Dhona	Saint Coinach's Find	(x29,y12)	71,040	410	Ilsabardian Bass	Deliver Ilsabardian Bass to Clifton. 0/3
+Sleeper Creeper	Sincerity	45	K'leytai	Mor Dhona	Saint Coinach's Find	(x29,y12)	32,572	468	Sludgeskipper	Deliver Sludgeskippers to Clifton. 0/3
+Putting the Zap on Nature	Concord	45	K'leytai	Mor Dhona	Saint Coinach's Find	(x29,y12)	35,520	482	Boltfish	Deliver Boltfish to Clifton. 0/3
+-->
       </div>
   </main>    
 </template>
