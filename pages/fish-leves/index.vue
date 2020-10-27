0d50d7408a9b5448050cea0bd640135f067da486
Julian <julianc@gmail.com>
2020-10-27 20:17:48 +1100

    use wrapper

--- pages/fish-leves/index.vue
+++ pages/fish-leves/index.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
       <h1 class="h2">Fishing Levequests</h1>
+      <div class="levequests-wrapper">
       <table class="levequests">
         <tr>
           <th>Guildeve</th>
@@ -18,5 +19,6 @@
           <td>Unsynwilf (x10, y8)</td>
           <td>Lower La Noscea - Limsa Lominsa</td>          
         </tr>
+      </div>
   </main>    
 </template>
