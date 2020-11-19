2ebe3421ffa6cee19ead2a52e3fafd0725a88355
Julian Chan <julianc@gmail.com>
2020-11-20 09:05:34 +1100

    remove timber and db pages

--- components/cnav.vue
+++ components/cnav.vue
@@ -3,9 +3,7 @@
     <div class="wrapper no-padding white-bg menu-bar">
       <nav>
         <ul>
-          <li><nuxt-link to="/fish-leves">Fishing Leves</nuxt-link></li>
-          <li><nuxt-link to="/fish-timers">Fishing Timers</nuxt-link></li>
-          <li><nuxt-link to="/fish-db">Fish Database</nuxt-link></li>
+          <li><nuxt-link to="/fish-leves">Fishing Leves</nuxt-link></li>          
         </ul>
       </nav>
       <clogin />
--- pages/fish-db/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <main>
-      <h1 class="h2">Fishing Database</h1>      
-  </main>    
-</template>
