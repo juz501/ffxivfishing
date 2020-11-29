627308ec8822e0fc33ffb989827a22a4defa3099
Julian Chan <julianc@gmail.com>
2020-11-29 12:58:38 +1100

    add quest req

--- pages/ocean-fishing/index.vue
+++ pages/ocean-fishing/index.vue
@@ -5,7 +5,9 @@
     <p>
       If want to get into fishing and don't have the patience for the regular chasing of fishing nodes,
       ocean fishing is for you.  Ocean fishing is great for both leveling and farming gatherer's
-      scrips.
+      scrips. To unlock Ocean Fishing, find Fhilsnoe (x7.8, y14.5) at Limsa Lominsa Lower Decks and
+      finish the short quest called 'All the Fish in the Sea' available at level 1 after obtaining
+      the Fisher Class.
     </p>
     <p>
       Ships depart regularly from the Ferry Docks at Limsa Lominsa Lower Decks every two hours.
@@ -24,7 +26,8 @@
     <h2 class="h3">Strategy and Baits</h2>
     <p>
       You will be going to 3 random destinations on your trip and each area has different fish. 
-      This means you need to be careful which bait you use at which location.
+      This means you need to be careful which bait you use at which location. Here is a list of
+      locations and bait to start off with in each location.
     </p>
 
     <table>
@@ -48,6 +51,10 @@
         <td>Southern Strait</td>
         <td>Krill</td>
       </tr>
-    </table>  
+    </table>
+    <p>
+        Once the Spectral rainbow event appears (it is triggered on an RNG by one person on the
+        boat fishing up a spectral fish), switch your bait to Plump Worm to get the best score.
+    </p>
   </main>
 </template>
